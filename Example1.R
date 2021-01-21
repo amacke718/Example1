2af72a5bdc67d2f104260dc7ead55ab656fada4e
amacke718 <mackeac@mail.uc.edu>
2021-01-21 16:10:03 -0500

    Amanda's Alterations

--- Example1.R
+++ Example1.R
@@ -1,3 +1,7 @@
+# This is Michal's Example
+# Other Notes:
+# "c" means "concatenate", which commonly means to link/chain objects together, which is what you do when you use c()
+
 file <- "TCGA_breast_cancer_LumA_vs_Basal_PAM50.tsv"
 first10 <- c('NAT1','BIRC5','BAG1','BCL2','BLVRA','CCNB1','CCNE1','CDC6','CDC20','CDH3')
 nfold <- 3
@@ -8,6 +12,12 @@ names(data) <- header
 
 header2 <- scan(file, skip = 1, nlines = 1, sep="\t", what = character())
 
+# dim(data) shows the dimension of the data 49 Columns & 329 Rows, but really thats including the headers, so it is 328 Rows
+# data$sample_id=='name of observation' will allow you to filter your data frame - show a particular header/value 
+# data[data$sample_id=='BCL2',] will pull out every data point in accordance with your filter
+# The use of the comma and space here, indicates "ROW FILTERING"
+# View(data) {The capital V here matters} - opens the data in a window
+# data[,c(6)]
 LumA <- data[data$sample_id %in% first10,header2=='Luminal A']
 Basal <- data[data$sample_id %in% first10,header2=='Basal-like']
 
